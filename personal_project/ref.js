8fc4471fa38ef85c180a730ca8b892dedafd3142
ammumike <saifiamaan445@gmail.com>
2021-04-26 15:15:03 +0530

    messageSend fxn added

--- personal_project/ref.js
+++ personal_project/ref.js
@@ -1,15 +1,19 @@
-let cheerio = require('cheerio');
-let request = require('request');
+// let cheerio = require('cheerio');
+// let request = require('request');
 const puppeteer = require('puppeteer');
 const id = "jafov95470@hype68.com";
 const pass = "20110073355";
-let jobId = "#####NO####ID###TESTING########";
-let jobLink = "####NO###LINK####TESTING#####"
+
+let companyName = process.argv.slice(2)[0];
+let jobId = process.argv.slice(2)[1];
+let jobLink = process.argv.slice(2)[2];
+
 let msg = `Hey There ! I am Mohd Aman , a third year student pursuing B.tech from GGSIPU and Currenty looking for internship roles , My key skills are - Strong Problem Solving, C, C++, Java OOPs, DBMS.
+I have completed 730+ questions on leetcode.
 It will be great If you can refer me for this role 
 Job id - ${jobId}
-Link- ${jobLink} Thank You`;
-let companyNames = process.argv.slice(2);
+Link- ${jobLink}
+Thank You`;
 (async () => {
   const browser = await puppeteer.launch({
       headless:false,
@@ -34,6 +38,23 @@ let companyNames = process.argv.slice(2);
     if(await page.$('.cp-card-new')!==null){
         page.click('.secondary-action');
     }
+    await page.waitForSelector('[data-link-to="mynetwork"]');
+    await page.click('[data-link-to="mynetwork"]');
+    await page.waitForSelector('.ember-view.mn-community-summary__link.link-without-hover-state');
+    let connectionButton = await page.$('.ember-view.mn-community-summary__link.link-without-hover-state');
+    await connectionButton.click();
+    await page.waitForSelector('[data-control-name="search_with_filters"]');
+    await page.click('[data-control-name="search_with_filters"]');
+    await page.waitForTimeout(3000);
+    await page.waitForSelector('.search-global-typeahead__input.always-show-placeholder');
+    await page.click('.search-global-typeahead__input.always-show-placeholder');
+    await page.type('.search-global-typeahead__input.always-show-placeholder',companyName)
+    await page.keyboard.press('Enter');
+    await page.waitForSelector('.entity-result__item');
+    let connectionLinks = await page.$$('.entity-result__item');
+    for(let i=0;i<connectionLinks.length;i++){
+        console.log(connectionLinks[i]);
+    }
     //code for searching for company 
     // for(let i=0;i<companyNames.length;i++){
     //     await page.waitForSelector('.search-global-typeahead__input.always-show-placeholder');
@@ -49,16 +70,23 @@ let companyNames = process.argv.slice(2);
     //     await page.waitForTimeout(10000);
     // }
     let links = ['https://www.linkedin.com/in/shantanu-11/','https://www.linkedin.com/in/mohd-aman/'];
-    for(let i=0;i<links.length;i++){
-    await page.goto(links[i]);
-    await page.waitForSelector('.message-anywhere-button.pvs-profile-actions__action.artdeco-button');
-    await page.click('.message-anywhere-button.pvs-profile-actions__action.artdeco-button');
-    await page.waitForSelector('.msg-form__contenteditable.t-14.t-black--light.t-normal.flex-grow-1.full-height.notranslate');
-    await page.type('.msg-form__contenteditable.t-14.t-black--light.t-normal.flex-grow-1.full-height.notranslate',msg);
-    await page.waitForSelector('.msg-form__send-button.artdeco-button.artdeco-button--1');
-    await page.waitForTimeout(5000);
-    await page.click('.msg-form__send-button.artdeco-button.artdeco-button--1');
-}
-
-//   await browser.close();
+    // await sendMessage(links,page);
+    // await browser.close();
 })();
+
+
+async function sendMessage(links,page){
+    for(let i=0;i<links.length;i++){
+        await page.goto(links[i]);
+        await page.waitForSelector('.message-anywhere-button.pvs-profile-actions__action.artdeco-button');
+        await page.click('.message-anywhere-button.pvs-profile-actions__action.artdeco-button');
+        await page.waitForTimeout(3000);
+        if(await page.$('.msg-s-message-list.full-width.scrollable')===null){
+            await page.waitForSelector('.msg-form__contenteditable.t-14.t-black--light.t-normal.flex-grow-1.full-height.notranslate');
+            await page.type('.msg-form__contenteditable.t-14.t-black--light.t-normal.flex-grow-1.full-height.notranslate',msg);
+            await page.waitForSelector('.msg-form__send-button.artdeco-button.artdeco-button--1');
+            await page.waitForTimeout(5000);
+            await page.click('.msg-form__send-button.artdeco-button.artdeco-button--1');
+        }
+    }
+}
