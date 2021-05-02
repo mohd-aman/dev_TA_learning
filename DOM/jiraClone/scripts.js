7e210959fc8962d518cf500da693df578757f851
ammumike <saifiamaan445@gmail.com>
2021-05-03 01:27:18 +0530

    modal added on click

--- DOM/jiraClone/scripts.js
+++ DOM/jiraClone/scripts.js
@@ -4,7 +4,7 @@ let filterCodes = {
     "green":"#2ecc71",
     "black":"#34495e"
 }
-
+let selectedFilter = "black";
 let allFilters = document.querySelectorAll('.ticket-filter div')
 // console.log(allFilters);
 let ticketContainer = document.querySelector('.tickets-container');
@@ -19,7 +19,20 @@ function handleModalBtn(e){
         return;
     }
     else{
-        let modalDiv = document.createElement('div');
+        let modalDiv = createModal();
+        modalDiv.querySelector(".modal-textbox").addEventListener("click",clearModalTextBox);
+        ticketContainer.append(modalDiv);
+        modalDiv.querySelector(".modal-textbox").addEventListener("keypress",addTickets);
+        let allModalFilters = modalDiv.querySelectorAll(".modal-filter");
+        for(let i=0;i<allModalFilters.length;i++){
+            allModalFilters[i].addEventListener("click",chooseModalFilter);
+        }
+        ticketContainer.append(modalDiv);
+    }
+}
+
+function createModal(){
+    let modalDiv = document.createElement('div');
         modalDiv.classList.add('modal');
         modalDiv.innerHTML = `<div class="modal-textbox" data-typed="false" contenteditable="true">
         Enter your task here
@@ -30,8 +43,30 @@ function handleModalBtn(e){
         <div class="modal-filter green"></div>
         <div class="modal-filter black"></div>
       </div>`;
-      modalDiv.querySelector(".modal-textbox").addEventListener("click",clearModalTextBox);
-      ticketContainer.append(modalDiv);
+      return modalDiv;
+}
+
+function chooseModalFilter(e){
+    let selectedModalFilter = e.target.classList[1];
+    if(selectedModalFilter == selectedFilter){
+        return;
+    }
+    selectedFilter = selectedModalFilter;
+    document.querySelector(".active-filter").classList.remove("active-filter");
+    e.target.classList.add("active-filter");
+}
+
+function addTickets(e){
+    if(e.key == 'Enter'){
+        let modalText = e.target.textContent;
+        let ticketDiv = document.createElement("div");
+        ticketDiv.classList.add("ticket");
+        ticketDiv.innerHTML = ` <div class="ticket-filter ${selectedFilter}"></div>
+        <div class="ticket-id">#exampleId</div>
+        <div class="ticket-content">${modalText}</div>`;
+        ticketContainer.append(ticketDiv);
+        e.target.parentNode.remove();
+        selectedFilter = "black";
     }
 }
 
