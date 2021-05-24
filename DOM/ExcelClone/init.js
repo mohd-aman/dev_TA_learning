6dacbc845ab7d8f83ec8c013c97376430fff76ff
ammumike <saifiamaan445@gmail.com>
2021-05-24 18:25:00 +0530

    ui update db init

---
+++ DOM/ExcelClone/init.js
@@ -0,0 +1,47 @@
+let cellsContentDiv = document.querySelector(".cells-content");
+
+function initCells(){
+    let cellsContent = "<div class='top-left-cell'></div>";
+    cellsContent+="<div class='top-row'>"
+    for(let i=0;i<26;i++){
+        cellsContent+=`<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`
+    }
+    cellsContent+="</div>"
+    cellsContent+="<div class='left-col'>"
+    for(let i=0;i<100;i++){
+        cellsContent+=`<div class="left-col-cell">${i+1}</div>`
+    }
+    cellsContent+="</div>"
+    cellsContent+="<div class='cells'>"
+    for(let i=0;i<100;i++){
+        cellsContent+="<div class='row'>"
+        for(let j=0;j<26;j++){
+            cellsContent+=`<div class='cell' rowid='${i}' colid='${j}' contentEditable='true'></div>`
+        }
+        cellsContent+="</div>"
+    }
+    cellsContent+="</div>"
+    cellsContentDiv.innerHTML = cellsContent;
+}
+
+initCells();
+
+let db;
+
+function initDB(){
+    db=[];
+    for(let i=0;i<100;i++){
+        let row = [];
+        for(let j=0;j<26;j++){
+            let name = String.fromCharCode(j+65)+(i+1)+"";
+            let cellObject = {
+                name:name,
+                value:""
+            }
+            row.push(cellObject);
+        }
+        db.push(row);
+    }
+}
+
+initDB();
--- DOM/ExcelClone/scripts.js
+++ DOM/ExcelClone/scripts.js
@@ -1,41 +1,36 @@
-let cellsContentDiv = document.querySelector(".cells-content");
-
-
-function initCells(){
-    let cellsContent = "<div class='top-left-cell'></div>";
-    cellsContent+="<div class='top-row'>"
-    for(let i=0;i<26;i++){
-        cellsContent+=`<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`
-    }
-    cellsContent+="</div>"
-    cellsContent+="<div class='left-col'>"
-    for(let i=0;i<100;i++){
-        cellsContent+=`<div class="left-col-cell">${i+1}</div>`
-    }
-    cellsContent+="</div>"
-    cellsContent+="<div class='cells'>"
-    for(let i=0;i<100;i++){
-        cellsContent+="<div class='row'>"
-        for(let j=0;j<26;j++){
-            cellsContent+="<div class='cell contentEditable='true'></div>"
-        }
-        cellsContent+="</div>"
-    }
-    cellsContent+="</div>"
-    cellsContentDiv.innerHTML = cellsContent;
-}
-
-initCells();
-
 let topRow = document.querySelector(".top-row");
 let leftCol = document.querySelector(".left-col");
 let topLeftCell = document.querySelector(".top-left-cell");
+let allCells = document.querySelectorAll(".cell");
+let addressInput = document.querySelector("#address");
 
 cellsContentDiv.addEventListener("scroll",function(e){
     let top = e.target.scrollTop;
     let left = e.target.scrollLeft;
+
     topRow.style.top = top+"px";
     topLeftCell.style.top = top+"px";
     topLeftCell.style.left = left+"px";
     leftCol.style.left = left+"px";
 })
+
+for(let i=0;i<allCells.length;i++){
+    allCells[i].addEventListener("click",function(e){
+        let rowId = Number(e.target.getAttribute("rowid"));
+        let colId = Number(e.target.getAttribute("colid"));
+        let address = String.fromCharCode(65+colId)+(rowId+1)+"";
+        addressInput.value = address;
+    })
+    
+    allCells[i].addEventListener("blur",function(e){
+        let cellValue = e.target.textContent;
+        let rowId = e.target.getAttribute("rowid");
+        let colId = e.target.getAttribute("colid");
+        let cellObject = db[rowId][colId];
+        if(cellObject.value == cellValue){
+            return;
+        }
+        cellObject.value = cellValue;
+        console.log("After update",cellObject)
+    })
+}
