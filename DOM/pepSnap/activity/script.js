f6ed9b863440b7eae1ba4b40aa0bc9fad642b98a
ammumike <saifiamaan445@gmail.com>
2021-06-02 19:22:19 +0530

    UI improved and ZoomIn & zoomOut implemented

--- DOM/pepSnap/activity/script.js
+++ DOM/pepSnap/activity/script.js
@@ -1,10 +1,16 @@
 let videoPlayer = document.querySelector("video");
 let recordButton = document.querySelector("#record-video");
 let photoButton = document.querySelector("#capture-photo");
+let zoomIn = document.querySelector("#in");
+let zoomOut = document.querySelector("#out");
 let recordingState = false;
 let constraints = {video:true};
 let recordedData;
 let mediaRecorder;
+
+let maxZoom = 3;
+let minZoom = 1;
+let currZoom = 1;
 
 (async function(){
 try{
@@ -34,16 +40,32 @@ try{
     recordButton.addEventListener("click",function(){
         if(recordingState){
             mediaRecorder.stop();
-            recordButton.innerHTML = "Record";
+            recordButton.querySelector("div").classList.remove("record-animate");
         }
         else{
             mediaRecorder.start();
-            recordButton.innerHTML = "Recording";
+            recordButton.querySelector("div").classList.add("record-animate");
         }
         recordingState = !recordingState;
     })
 
     photoButton.addEventListener("click",capturePhotos);
+
+    zoomIn.addEventListener("click",function(){
+        if(currZoom+0.1<=maxZoom){
+            currZoom+=0.1;
+            videoPlayer.style.transform = `scale(${currZoom})`;
+        }
+    });
+
+    zoomOut.addEventListener("click",function(){
+        if(currZoom-0.1>=minZoom){
+            currZoom-=0.1;
+            videoPlayer.style.transform = `scale(${currZoom})`;
+        }
+    });
+
+
 }
     catch(error){
         console.log(error);
@@ -62,10 +84,20 @@ function saveVideoToFs(){
 }
 
 function capturePhotos(){
+    photoButton.querySelector("div").classList.add("capture-animate");
+    setTimeout(function(){
+        photoButton.querySelector("div").classList.remove("capture-animate");
+    },1000);
     let canvas = document.createElement("canvas");
     canvas.height = videoPlayer.videoHeight;
     canvas.width = videoPlayer.videoWidth;
     let ctx = canvas.getContext("2d");
+
+    if(currZoom!=1){
+        ctx.translate(canvas.width/2,canvas.height/2);
+        ctx.scale(currZoom,currZoom);
+        ctx.translate(-canvas.width/2,-canvas.height/2);
+    }
     ctx.drawImage(videoPlayer,0,0);
     let imageUrl = canvas.toDataURL("image/url");
     let aTag = document.createElement("a");
