bbcda5a803c6e7bfb64f07590d524feb76793676
ammumike <saifiamaan445@gmail.com>
2021-06-02 14:44:57 +0530

    Media Recorder

--- DOM/pepSnap/activity/script.js
+++ DOM/pepSnap/activity/script.js
@@ -1,9 +1,43 @@
 let videoPlayer = document.querySelector("video");
+let recordButton = document.querySelector("#record-video");
+let photoButton = document.querySelector("#capture-photo");
+let recordingState = false;
 let constraints = {video:true};
+let recordedData;
+let mediaRecorder;
 
 (async function(){
-
+try{
     let mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
-    console.log(mediaStream);
+    // console.log(mediaStream);
     videoPlayer.srcObject = mediaStream;
+    mediaRecorder = new MediaRecorder(mediaStream);
+
+    mediaRecorder.onstop = function(e){
+        console.log("Inside on stop");
+    }
+    mediaRecorder.onstart = function(e){
+        console.log("Inside on start");
+    }
+    mediaRecorder.ondataavailable = function(e){
+        console.log("Inside on data availble");
+        recordedData = e.data;
+    }
+    console.log(mediaRecorder);
+
+    recordButton.addEventListener("click",function(){
+        if(recordingState){
+            mediaRecorder.stop();
+            recordButton.innerHTML = "Record";
+        }
+        else{
+            mediaRecorder.start();
+            recordButton.innerHTML = "Recording";
+        }
+        recordingState = !recordingState;
+    })
+}
+    catch(error){
+        console.log(error);
+    }
 })();
