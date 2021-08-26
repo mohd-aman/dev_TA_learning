040cbf3da51fc26fd290bd3f337047ac5f635dcf
ammumike <saifiamaan445@gmail.com>
2021-08-26 15:17:59 +0530

    reels upload added

--- React/reels/src/Firebase.jsx
+++ React/reels/src/Firebase.jsx
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import config from "./config.json";
 import "firebase/auth"
 import "firebase/firestore"
+import "firebase/storage"
 
 firebase.initializeApp(config);
 
@@ -9,6 +10,7 @@ let provider = new firebase.auth.GoogleAuthProvider();
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+export const storage = firebase.storage();
 
 export const signInWithGoogle = ()=>{
     auth.signInWithPopup(provider);
